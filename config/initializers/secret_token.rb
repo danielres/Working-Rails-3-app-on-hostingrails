eb6c14d8b99554dbab73049fa8ddc27ee326bacb
danielres <nospam2010@hotmail.fr>
2010-12-31 05:45:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HowtosayRails3::Application.config.secret_token = 'b07fe2069a2965d81a0deb4141c92db741c3d8638e34dbaabac5b31c7eaf3cd4359855c0ef9b88e54a8fe2f31eaa5b699312e34d123abb9b961ef20b41911954'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HowtosayRails3::Application.config.session_store :cookie_store, :key => '_howtosay_rails3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# HowtosayRails3::Application.config.session_store :active_record_store
